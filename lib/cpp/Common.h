b387bac215614c1c462d3304f94e58885de939ba
zhangshuai <zs2619@gmail.com>
2012-05-15 19:56:11 +0800

    bug

--- lib/cpp/Common.h
+++ lib/cpp/Common.h
@@ -13,7 +13,7 @@ typedef unsigned char 	uint8;
 typedef signed   char 	int8;
 
 typedef unsigned short 	uint16;
-typedef signed	 char 	int16;
+typedef signed	 short 	int16;
 
 
 typedef unsigned int 	uint32;
