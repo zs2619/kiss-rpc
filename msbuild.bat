3d3d99f81308be625331726c573c9d47cfee09e3
zhangshuai <zs2619@gmail.com>
2018-12-10 16:32:34 +0800

    msbuild bat

--- msbuild.bat
+++ msbuild.bat
@@ -1,3 +1,8 @@
-set VCTargetsPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets"
+
+set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
+set VCVARS=%VS%\VC\Auxiliary\Build\vcvars32.bat
+echo %VCVARS%
+call "%VCVARS%"
+set VCTargetsPath=%VS%\Common7\IDE\VC\VCTargets
 MSBuild.exe rpc.sln  /p:Configuration=Release /p:Platform=X86
 MSBuild.exe rpc.sln  /p:Configuration=Debug /p:Platform=X86
